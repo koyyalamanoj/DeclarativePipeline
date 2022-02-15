6f9b3c2c0e2b752ca08148b2873f687f8535009e
manoj <manoj@gmail.com>
2022-02-15 12:24:35 +0530

    hell

--- src/com/mercedesbenz/yacto/BasePipelineType.groovy
+++ src/com/mercedesbenz/yacto/BasePipelineType.groovy
@@ -1,21 +1,18 @@
-package com.mercedesbenz.yacto;
+package com.mercedesbenz.yacto
 
 
 
 abstract class BasePipelineType  {
 
-    
+    def testcommand = "default test command"
     def dockerFilePath = ''
     def dockerFileName = ''
   
-    
-    
     abstract protected void buildStesps()
     abstract protected void dockerBuild()
     abstract protected void runTestcommand()
 
-    void startPipeline(){
+    def startPipeline(){
         this.build()
-        
     }
 }
