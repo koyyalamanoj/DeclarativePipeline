10a58a2c12731c9ad1757736e4231e5eef2da962
manoj <manoj@gmail.com>
2022-02-15 11:47:49 +0530

    hellos

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,13 +1,13 @@
 @Library('TestLibrary@main')
 import com.mercedesbenz.yacto.YactoPipeline;
-
+node {
 def buildType = new YactoPipeline(
       dockerImage:'windows',
       scmtype:'gitlab',
       );
 
 buildType.startPipeline()
-
+}
 
 
 
